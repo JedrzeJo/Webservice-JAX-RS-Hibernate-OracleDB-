d2b29710a826bcc4d2c71d03cf618ae9d593afb5
Jedrzej <34516404+JedrzeJo@users.noreply.github.com>
2018-04-16 12:33:27 +0200

    Pagination and Filtering by Year options implemented ( in MessageService and MessageResource classes).

--- messager/src/main/java/org/jedrzej/messager/messager/service/MessageService.java
+++ messager/src/main/java/org/jedrzej/messager/messager/service/MessageService.java
@@ -1,6 +1,7 @@
 package org.jedrzej.messager.messager.service;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 
@@ -12,13 +13,39 @@ public class MessageService {
 	private Map<Long, Message> messages = DatabaseClass.getMessages();
 
 	public MessageService() {
-		messages.put(1L, new Message(1L, "dupa jasiu ", "Jedrzej"));
-		messages.put(2L, new Message(2L, "pozdrawiam tibiasza ", "Cyc"));
+		messages.put(1L, new Message(1L, "dupa", "Jedrzej"));
+		messages.put(2L, new Message(2L, "pozdrawiam mariusza", "Cyc"));
 	}
 	
 	public List <Message> getAllMessages(){
 		return new ArrayList<Message>(messages.values());
 	}
+	
+	
+	
+	public List <Message> getAllMessagesByYear (int year){
+		List <Message> messagesForYear = new ArrayList<>();
+		Calendar cal = Calendar.getInstance();
+		for (Message message : messages.values()) {
+			cal.setTime(message.getCreation_date());
+			if(cal.get(Calendar.YEAR) == year) {
+				messagesForYear.add(message);
+			}
+		}
+		return messagesForYear;
+	}
+	
+	
+	
+	public List <Message> getAllMessagesPaginated(int start, int size){
+		ArrayList<Message> list = new ArrayList<>(messages.values());
+		if(start + size > list.size()) {
+			return new ArrayList <Message>();
+		}
+		return list.subList(start, start+size);
+	}
+	
+	
 	
 	public Message getMessage(long id) {
 		return messages.get(id);
